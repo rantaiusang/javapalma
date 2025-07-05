b1961fdb54ef015f2c398ea6bd616ed9a8315812
rantaiusang <baramultijavapalma@gmail.com>
2025-07-05 15:16:35 +0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-};
+}
