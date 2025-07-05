a5b1bed20847720588a6f35c50f6d7fd73d6da87
rantaiusang <baramultijavapalma@gmail.com>
2025-07-05 16:02:01 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
-import './style.css'; // Import Tailwind
+import App from './App';
+import './style.css'; // ✅ Import Tailwind + custom CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
